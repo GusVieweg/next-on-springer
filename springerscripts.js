b0a5bc5fa7ce440c1d4620bbc273ad7b0de049ce
Augustus Vieweg <awvieweg@ncsu.edu>
2017-09-12 22:28:11 -0400

    replace getjson with ajax

--- springerscripts.js
+++ springerscripts.js
@@ -10,30 +10,62 @@ function capitalizeFirstLetter(string) {
 }
 
 function generateWordsFromSuggestion(suggestion, i) {
-	wordsAPI = "https://api.datamuse.com/sug?s=" + suggestion[i] + '&max=50'
-	$.getJSON( wordsAPI, function( res ) {
-		if( i == suggestion.length ) {
-			if( Math.random() > 0.5 ) {
-				$('#lol').append('.')
+	wordsAPI = "http://api.datamuse.com/sug?s=" + suggestion[i] + '&max=50'
+
+	$.ajax({
+	    url: wordsAPI,
+	    // headers: { 'Access-Control-Allow-Origin': '*' },
+	    crossDomain: true,
+	    success: function( res ) {
+			if( i == suggestion.length ) {
+				if( Math.random() > 0.5 ) {
+					$('#lol').append('.')
+				} else {
+					$('#lol').append('?')
+				}
+				// console.log("Word complete!")
 			} else {
-				$('#lol').append('?')
+				// console.log("Nice")
+				place = Math.floor(Math.random() * 50)
+				word = res[place].word
+				if( i == 0 ) {
+					word = capitalizeFirstLetter(word)
+				}
+				$('#lol').append(word)
+				if( i < suggestion.length - 1 ) {
+					$('#lol').append(' ')
+				}
+				j = i + 1
+				generateWordsFromSuggestion(suggestion, j)
 			}
-			// console.log("Word complete!")
-		} else {
-			// console.log("Nice")
-			place = Math.floor(Math.random() * 50)
-			word = res[place].word
-			if( i == 0 ) {
-				word = capitalizeFirstLetter(word)
-			}
-			$('#lol').append(word)
-			if( i < suggestion.length - 1 ) {
-				$('#lol').append(' ')
-			}
-			j = i + 1
-			generateWordsFromSuggestion(suggestion, j)
-		}
-	})
+		},
+	    error: function() {alert('it doesnt work')},
+	    datatype: 'jsonp'
+	});
+
+	// $.getJSON( wordsAPI, function( res ) {
+	// 	if( i == suggestion.length ) {
+	// 		if( Math.random() > 0.5 ) {
+	// 			$('#lol').append('.')
+	// 		} else {
+	// 			$('#lol').append('?')
+	// 		}
+	// 		// console.log("Word complete!")
+	// 	} else {
+	// 		// console.log("Nice")
+	// 		place = Math.floor(Math.random() * 50)
+	// 		word = res[place].word
+	// 		if( i == 0 ) {
+	// 			word = capitalizeFirstLetter(word)
+	// 		}
+	// 		$('#lol').append(word)
+	// 		if( i < suggestion.length - 1 ) {
+	// 			$('#lol').append(' ')
+	// 		}
+	// 		j = i + 1
+	// 		generateWordsFromSuggestion(suggestion, j)
+	// 	}
+	// })
 }
 
 function improvise() {
