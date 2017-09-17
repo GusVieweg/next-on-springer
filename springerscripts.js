aee6800f7e8e1405dd9050b1495dc5fe608e2025
Augustus Vieweg <awvieweg@ncsu.edu>
2017-09-17 16:08:45 -0400

    slider, storage, and bug fixes

--- springerscripts.js
+++ springerscripts.js
@@ -1,10 +1,10 @@
 var interval = null;
-
+var suggo = ''
 function getFormData() {
-	var suggestion = $('#suggestionText')[0].value
+	suggo = $('#suggestionText')[0].value
 	$('#suggestionText')[0].value = ''
 	$('#suggestionGen').text('')
-	return suggestion
+	return suggo
 }
 
 function capitalizeFirstLetter(string) {
@@ -38,6 +38,8 @@ function generateWordsFromSuggestion(suggestion, i, prevpos) {
 					case 5: $('#suggestionGen').append(' ;)'); break;
 					case 6: $('#suggestionGen').append(' :)'); break;
 				}
+				$('#cSaveForm').show();
+				$('#cPlayAgainForm').show();
 			} else {
 				// console.log("Nice")
 				var place = Math.floor(Math.random() * 50)
@@ -70,8 +72,12 @@ function generateWordsFromSuggestion(suggestion, i, prevpos) {
 					    								console.log(res[k].word + ", which is a " + res[k].tags[m] + ", selected to follow a " + prevpos)
 					    								true_word = res[k].word;
 					    								prevpos = res[k].tags[m];
-					    								if( prevpos == 'v' ) {
-					    									true_word = true_word + 's'
+					    								if( (prevpos == 'v') ) {
+					    									if(true_word[-1] !== 's') {
+					    										true_word = true_word + 's'
+					    									} else {
+					    										true_word = true_word + 'es'
+					    									}
 					    								}
 					    								console.log("---------------- FORCING BREAK ----------------")
 					    								force_break = true;
@@ -86,7 +92,7 @@ function generateWordsFromSuggestion(suggestion, i, prevpos) {
 		    				if( !force_break ) {
 		    					console.log("Did not find ANY words!")
 		    					true_word = "..." + res[0].word;
-		    					if( res[0].tags !== "undefined" ) {
+		    					if( typeof res[0].tags !== "undefined" ) {
 		    						prevpos = res[0].tags[0];
 		    					} else {
 		    						prevpos = ""
@@ -95,12 +101,13 @@ function generateWordsFromSuggestion(suggestion, i, prevpos) {
 		    			}
 	    				if( i == 0 ) {
 							true_word = capitalizeFirstLetter(word)
-							if( res[0].tags !== "undefined" ) {
+							if( typeof res[0].tags !== "undefined" ) {
 	    						prevpos = res[0].tags[0];
 	    					} else {
 	    						prevpos = ""
 	    					}
 						}
+						true_word = true_word.replace(" ", "-");
 						$('#suggestionGen').append(true_word)
 						if( i < suggestion.length - 1 ) {
 							$('#suggestionGen').append(' ')
@@ -135,9 +142,9 @@ function generateRandomWord(playerName) {
 		success: function( res ) {
 			var randomPlace = Math.floor(Math.random() * 75)
 			var randomWord = res[randomPlace].word
-			if( randomWord.length <= 8 ) {
-				console.log(playerName)
-				var textInsert = "Okay, " + playerName + ". Your word is: <br />" + randomWord + '.'
+			if( (randomWord.length <= 8) && (randomWord.length > 1) ) {
+				// console.log(playerName)
+				var textInsert = "Okay... Your word is: <p id='randoWord'>" + randomWord + '</p>'
 				$('#nameGen').show();
 				$('#nameGen').html(textInsert)
 				startTimer(20, $('#time'))
@@ -159,11 +166,16 @@ function getNameData() {
 
 function askUser() {
 	clearInterval(interval)
+	$('#playAgainForm').show();
+	$('#saveForm').show();
 	$('#answerForm').hide();
 	var input = $('#answerText')[0].value
 	var leftoverTime = parseInt($('#time').text())
 	$('#time').hide();
 	$('#results').text("You entered '" + input + "' in " + String(20-leftoverTime) + " seconds!" )
+	$('#results').show();
+	$('#saveToVault').prop('disabled', false);
+	$('#saveToVault')[0].innerText = "Save to vault"
 }
 
 function countdown() {
@@ -187,4 +199,149 @@ function playAgain() {
 	$('#results').hide();
 	$('#nameGen').hide();
 	$('#nameForm').show();
+	$('#saveForm').hide();
+	$('#playAgainForm').hide();
+	$('#answerText')[0].value = ''
 }
+
+function cPlayAgain() {
+	$('#suggestionForm').show();
+	$('#cSaveForm').hide();
+	$('#cPlayAgainForm').hide();
+	$('#suggestionText')[0].value = ''
+	$('#suggestionGen')[0].innerText = ''
+	$('#cSaveToVault').prop('disabled', false);
+	$('#cSaveToVault')[0].innerText = "Save to vault"
+}
+
+function clearDatabase() {
+	const proxyurl = "https://cors-anywhere.herokuapp.com/";
+	var storageAPI = "https://api.myjson.com/bins/qidrh";
+	$.ajax({
+		url: storageAPI,
+		type: "PUT",
+		crossDomain: true,
+		data: '[]',
+		contentType: "application/json; charset=utf-8",
+		success: function( res ) {
+			$('#saveToVault')[0].innerText = "Saved!"
+		},
+		error: function() {alert('it doesnt work')},
+		datatype: 'jsonp'
+	})
+}
+
+function saveToVault() {
+	$('#saveToVault').prop('disabled', true);
+	$('#saveToVault')[0].innerText = "Saving..."
+	var playerName = $('#nameText')[0].value
+	var answer = $('#answerText')[0].value
+	var seconds = parseInt($('#time').text())
+	var prompt = $('#randoWord')[0].innerText
+	var new_entry = {}
+	const proxyurl = "https://cors-anywhere.herokuapp.com/";
+	var storageAPI = "https://api.myjson.com/bins/qidrh";
+	new_entry.player = playerName
+	new_entry.answer = answer
+	new_entry.seconds = 20 - seconds
+	new_entry.prompt = prompt
+	$.ajax({
+		url: proxyurl + storageAPI,
+		type: "GET",
+		headers: { 'Access-Control-Allow-Origin': '*' },
+		crossDomain: true,
+		success: function( res ) {
+			console.log(res)
+			new_data = res
+			new_data.push(new_entry)
+			if( new_data.length > 5 ) {
+				new_data.splice(0, 1)
+			}
+			stringit = JSON.stringify(new_data)
+			$.ajax({
+				url: storageAPI,
+				type: "PUT",
+				data: stringit,
+				contentType: "application/json; charset=utf-8",
+				crossDomain: true,
+				success: function( res ) {
+					$('#saveToVault')[0].innerText = "Saved!"
+					initializeCarousel();
+				},
+				error: function() {alert('it doesnt work')},
+				datatype: 'jsonp'
+			})
+		},
+		error: function() {alert('it doesnt work')},
+		datatype: 'jsonp'
+	})
+}
+
+function cSaveToVault() {
+	$('#cSaveToVault').prop('disabled', true);
+	$('#cSaveToVault')[0].innerText = "Saving..."
+	var playerName = "Computer generated"
+	var answer = $('#suggestionGen')[0].innerText
+	var prompt = suggo
+	var new_entry = {}
+	const proxyurl = "https://cors-anywhere.herokuapp.com/";
+	var storageAPI = "https://api.myjson.com/bins/qidrh";
+	new_entry.player = playerName
+	new_entry.answer = answer
+	new_entry.seconds = "a few"
+	new_entry.prompt = prompt
+	$.ajax({
+		url: proxyurl + storageAPI,
+		type: "GET",
+		headers: { 'Access-Control-Allow-Origin': '*' },
+		crossDomain: true,
+		success: function( res ) {
+			console.log(res)
+			new_data = res
+			new_data.push(new_entry)
+			if( new_data.length > 5 ) {
+				new_data.splice(0, 1)
+			}
+			stringit = JSON.stringify(new_data)
+			$.ajax({
+				url: storageAPI,
+				type: "PUT",
+				data: stringit,
+				contentType: "application/json; charset=utf-8",
+				crossDomain: true,
+				success: function( res ) {
+					$('#cSaveToVault')[0].innerText = "Saved!"
+					initializeCarousel();
+				},
+				error: function() {alert('it doesnt work')},
+				datatype: 'jsonp'
+			})
+		},
+		error: function() {alert('it doesnt work')},
+		datatype: 'jsonp'
+	})
+}
+
+function initializeCarousel() {
+	const proxyurl = "https://cors-anywhere.herokuapp.com/";
+	var storageAPI = "https://api.myjson.com/bins/qidrh";
+	$.ajax({
+		url: proxyurl + storageAPI,
+		type: "GET",
+		headers: { 'Access-Control-Allow-Origin': '*' },
+		crossDomain: true,
+		success: function( res ) {
+			var i = 0
+			for( i=0 ; i<5 ; i++ ) {
+				id = '#slide-' + String(i) + '-header'
+				$(id)[0].innerText = res[i].answer
+				id2 = '#slide-' + String(i) + '-content'
+				$(id2)[0].innerText = '-- ' + res[i].player + ' with prompt "' + res[i].prompt + '" in ' + String(res[i].seconds) + ' seconds'
+				console.log($(id2))
+			}
+		},
+		error: function() {alert('it doesnt work')},
+		datatype: 'jsonp'
+	})
+}
+
